c521951a128c4823a4dffe6ac1fc49acd1b5cac3
James Senva <jamessenva@gmail.com>
2023-09-11 22:25:25 +0530

    Return the new dice position

---
+++ 8 kyu/grasshoper-terminal-game.js
@@ -0,0 +1,36 @@
+'use strict';
+
+// console.log('hello')
+
+// NAME OF THE KATA : Grasshopper - Terminal game move function
+// URL : https://www.codewars.com/kata/563a631f7cbbc236cf0000c2/train/javascript
+
+
+/*
+
+INSTRUCTIONS :
+Terminal game move function
+
+In this game, the hero moves from left to right. The player rolls the dice and moves the number of spaces indicated by the dice two times.
+
+Create a function for the terminal game that takes the current position of the hero and the roll (1-6) and return the new position.
+Example:
+
+move(3, 6) should equal 15
+
+*/
+
+
+
+// SOLUTION
+function move (position, roll) {
+  console.log(roll * 2 + position);
+}
+
+
+
+
+// SAMPLE TESTS
+move(0, 4); // 8
+move(3, 6); // 15
+move(2, 5); // 12
